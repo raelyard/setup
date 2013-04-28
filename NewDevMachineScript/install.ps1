5b803405ceb2d9a6bc2be0e5f867d0f7b35373f8
Unknown <dave@raelyard.com>
2013-04-28 15:11:16 -0600

    changed order of install for new machine such that use web platform installer first to get some dependencies in place.  because .net 4 is a dependency for chocolatey, need to use web platform installer to get that in place before getting started with chocolatey

--- NewDevMachineScript/install.ps1
+++ NewDevMachineScript/install.ps1
@@ -1,36 +1,42 @@
-.\InstallSqlServer.cmd
+.\InstallWebPlatformInstaller.ps1
+
+# IIS
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:NetFramework4"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:NetFramework45"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:IIS7"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:ASPNET"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:BasicAuthentication"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:DefaultDocument"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:DigestAuthentication"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:DirectoryBrowse"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:HTTPErrors"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:HTTPLogging"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:HTTPRedirection"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:IIS7_ExtensionLessURLs"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:IISManagementConsole"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:IPSecurity"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:ISAPIExtensions"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:ISAPIFilters"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:LoggingTools"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:MetabaseAndIIS6Compatibility"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:NETExtensibility"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:RequestFiltering"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:RequestMonitor"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:StaticContent"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:StaticContentCompression"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:Tracing"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:WindowsAuthentication"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:UrlRewrite2"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:MVC4"
+
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:WindowsAzureSDK"
+cmd /C ".\WebPlatformInstaller\webpicmd /install /AcceptEula /SuppressReboot /Products:WindowsAzureToolsVS2012"
+
 
 # Chocolatey stuff
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 
 cinst .\packages.config
 
-# IIS
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:IIS7"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:ASPNET"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:BasicAuthentication"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:DefaultDocument"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:DigestAuthentication"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:DirectoryBrowse"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:HTTPErrors"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:HTTPLogging"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:HTTPRedirection"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:IIS7_ExtensionLessURLs"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:IISManagementConsole"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:IPSecurity"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:ISAPIExtensions"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:ISAPIFilters"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:LoggingTools"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:MetabaseAndIIS6Compatibility"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:NETExtensibility"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:RequestFiltering"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:RequestMonitor"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:StaticContent"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:StaticContentCompression"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:Tracing"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:WindowsAuthentication"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:UrlRewrite2"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:MVC4"
 
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:WindowsAzureSDK"
-cmd /C "webpicmd /install /AcceptEula /SuppressReboot /Products:WindowsAzureToolsVS2012"
+.\InstallSqlServer.cmd

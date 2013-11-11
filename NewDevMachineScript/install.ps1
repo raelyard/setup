96df3ab2526aa30f2363af146118a442c9f750db
Unknown <dave@raelyard.com>
2013-11-11 11:00:27 -0800

    got shouting menu text removed from visual studio 2013 in addition to 2012

--- NewDevMachineScript/install.ps1
+++ NewDevMachineScript/install.ps1
@@ -42,6 +42,7 @@ cinst WindowsAzureToolsVS2012 -source webpi
 
 # fix visual studio menus shouting: http://stackoverflow.com/questions/10859173/how-to-disable-all-caps-menu-titles-in-visual-studio
 Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\11.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\12.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
 
 #get visualhg showing up as a source control option in visual studio: http://www.skimedic.com/blog/post/2012/09/27/Fixing-the-Visual-Studio-2012-and-VisualHG-Installation-Issue.aspx
 & "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv" /setup

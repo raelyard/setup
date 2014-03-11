d2ad750a8fd288ff92f5c50ab8275064ccf8c27c
raelyard <unknown>
2014-03-11 14:06:53 -0600

    added function to handle getting both posh git and posh hg working together

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -134,4 +134,46 @@ if(test-path $aliasesDirectory)
 "  scripts:" + $scripts
 "  Imported C:\Program Files\Microsoft Visual Studio 12.0\vc\vcvarsall.bat"
 "_______________________________________________________________________________"
-
+function isCurrentDirectoryARepository($type) {
+ 
+    if ((Test-Path $type) -eq $TRUE) {
+        return $TRUE
+    }
+ 
+    # Test within parent dirs
+    $checkIn = (Get-Item .).parent
+    while ($checkIn -ne $NULL) {
+        $pathToTest = $checkIn.fullname + '/' + $type;
+        if ((Test-Path $pathToTest) -eq $TRUE) {
+            return $TRUE
+        } else {
+            $checkIn = $checkIn.parent
+        }
+    }
+    return $FALSE
+}
+ 
+# Posh-Hg and Posh-git prompt
+# Change this to point to wherever your two profile scripts are
+ 
+. 'C:\Code\OpenSource\posh-hg\profile.example.ps1'
+. 'C:\Code\OpenSource\posh-git\profile.example.ps1'
+ 
+function prompt(){
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+ 
+    Write-Host($pwd) -nonewline
+ 
+    if (isCurrentDirectoryARepository(".git")) {
+        # Git Prompt
+        $Global:GitStatus = Get-GitStatus
+        Write-GitStatus $GitStatus
+    } elseif (isCurrentDirectoryARepository(".hg")) {
+        # Mercurial Prompt
+        $Global:HgStatus = Get-HgStatus
+        Write-HgStatus $HgStatus
+    }
+ 
+    return "> "
+}

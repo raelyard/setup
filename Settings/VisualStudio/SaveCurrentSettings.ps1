a514862ec429d9654db2bc7069abef9f26b1e06d
raelyard <unknown>
2014-03-18 16:02:19 -0600

    added scripts for setting visual studio settings

---
+++ Settings/VisualStudio/SaveCurrentSettings.ps1
@@ -0,0 +1,1 @@
+copy-item "$env:userprofile\Documents\Visual Studio 2012\Settings\CurrentSettings.vssettings" ".\2012\CurrentSettings.vssettings"
---
+++ Settings/VisualStudio/SetVisualStudioSettings.ps1
@@ -0,0 +1,1 @@
+copy-item ".\2012\CurrentSettings.vssettings" "$env:userprofile\Documents\Visual Studio 2012\Settings\CurrentSettings.vssettings"

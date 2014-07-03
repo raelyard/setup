8952f04ae89ad60689bb02d6b3dbcf6930fd0565
Dave Rael <dave@raelyard.com>
2014-07-03 05:45:20 -0600

    Added a function to conveniently pop open the hosts file to be able to make edits without navigating the file system

--- Powershell/Functions/Utilities.ps1
+++ Powershell/Functions/Utilities.ps1
@@ -1,2 +1,3 @@
 function Edit-Profile{np $profile}
 function locateCommand{ get-command $args | format-list * }
+function hosts{ & "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:windir\system32\drivers\etc\hosts }

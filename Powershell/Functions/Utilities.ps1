4c9bd16b75b3136f78f7e905afb7385c323f1b86
Dave Rael <dave@raelyard.com>
2014-11-18 09:57:21 -0700

    Added function and aliases for poppping open machine.config in notepad++

--- Powershell/Functions/Utilities.ps1
+++ Powershell/Functions/Utilities.ps1
@@ -1,3 +1,4 @@
 function Edit-Profile{np $profile}
 function locateCommand{ get-command $args | format-list * }
 function hosts{ & "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:windir\system32\drivers\etc\hosts }
+function machineConfig{ & "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:windir\Microsoft.NET\Framework64\v4.0.30319\Config\machine.config }

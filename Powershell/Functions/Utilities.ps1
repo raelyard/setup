5e6827d225863b93d2f0fdb74b00043f0da1c2b9
Dave Rael <unknown>
2017-11-15 15:30:33 -0700

    Updated installation path of NotePad++
    to reflect changed path where it seems to be installed

--- Powershell/Functions/Utilities.ps1
+++ Powershell/Functions/Utilities.ps1
@@ -1,7 +1,7 @@
 function Edit-Profile{np $profile}
 function locateCommand{ get-command $args | format-list * }
-function hosts{ & "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:windir\system32\drivers\etc\hosts }
-function machineConfig{ & "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:windir\Microsoft.NET\Framework64\v4.0.30319\Config\machine.config }
+function hosts{ & "C:\Program Files\Notepad++\notepad++.exe" $env:windir\system32\drivers\etc\hosts }
+function machineConfig{ & "C:\Program Files\Notepad++\notepad++.exe" $env:windir\Microsoft.NET\Framework64\v4.0.30319\Config\machine.config }
 function clipX
 {
 	if((get-process clipx -ErrorAction SilentlyContinue) -eq $null)

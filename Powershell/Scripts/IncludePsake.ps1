c1aaf9e7cda3e0c898111f19b2c2b53675655eff
dave@raelyard.com <unknown>
2019-01-09 20:02:25 -0700

    Got rid of the inapprpriately included version of psake
    that was in this repository and included in PowerShell profile
    (and getting a bit in the way of other scripts I was running)

--- Powershell/Scripts/IncludePsake.ps1
+++
@@ -1,4 +0,0 @@
-$psakeDirectory = "$setupDirectory\psake"
-function initialize-psake {Import-Module $psakeDirectory\PSake.psm1;}
-
-initialize-psake
--- Powershell/SetPowerShellProfile.ps1
+++ Powershell/SetPowerShellProfile.ps1
@@ -1,6 +1,3 @@
 $profileDirectory = split-path $profile
 mkdir -force $profileDirectory
 cp ./Microsoft.PowerShell_profile.ps1 $profileDirectory
-
-mkdir -force $profileDirectory\Modules\psake
-cp ..\psake\psake.psm1 $profileDirectory\Modules\psake

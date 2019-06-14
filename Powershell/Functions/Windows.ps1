e14571eb528d356dc32b6121a5ff300102597426
dave@raelyard.com <unknown>
2019-06-13 18:23:43 -0600

    Add function to set environment variable persistently
    so I can stop forgetting how to do this and just make it easy

--- Powershell/Functions/Windows.ps1
+++ Powershell/Functions/Windows.ps1
@@ -10,3 +10,15 @@ function Append-Path
     $currentPath.Add($newPathEntry)
 	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value ($currentPath -join ';')
 }
+
+function Set-EnvironmentVariable
+{
+    param([string]$name, [string]$value, [bool]$systemScope)
+
+    $scope = [System.EnvironmentVariableTarget]::User
+    if($systemScope) {
+        $scope = [System.EnvironmentVariableTarget]::Machine
+    }
+
+    [System.Environment]::SetEnvironmentVariable("$NAME", "$value", $scope)
+}
